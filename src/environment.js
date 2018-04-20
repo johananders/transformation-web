e4d35cf5936c85eb6a2175336eea40af7caaf839
Johan Andersson <johan.andersson@izettle.com>
2018-04-20 08:27:30 +0200

    static.json

--- src/environment.js
+++ src/environment.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV;
 
 const apiVersion = 'v1';
 
-let backendHost = env === 'production' ? '/api/' : 'http://localhost:8080';
+let backendHost = env === 'production' ? 'https://reverse-service.herokuapp.com' : 'http://localhost:8080';
 
 const API_ROOT = backendHost + '/transformations/' + apiVersion;
 
